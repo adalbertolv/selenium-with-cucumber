b8a946c44ce5a71992c8cd6b200f729569d20730
Adalberto Lopez Venegas <adalberto.lopez@astrazeneca.com>
2017-04-06 03:15:08 -0500

    Added TestNG back
    Signed-off-by: Adalberto Lopez Venegas <adalberto.lopez@astrazeneca.com>

--- src/test/java/runner/TestRunner.java
+++ src/test/java/runner/TestRunner.java
@@ -4,10 +4,11 @@ import org.junit.runner.RunWith;
 
 import cucumber.api.CucumberOptions;
 import cucumber.api.junit.Cucumber;
+import cucumber.api.testng.AbstractTestNGCucumberTests;
 
 @RunWith(Cucumber.class)
 @CucumberOptions(features = {"src/test/java/features"} , plugin = {"pretty", "json:target/cucumber.json","html:target/cucumber-html-report"},
         glue = "classpath:steps")
-public class TestRunner {
+public class TestRunner extends AbstractTestNGCucumberTests {
 
 }
